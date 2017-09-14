15b2a5287847d214bdd56bb79b49bfd849639854
Ernests Millers <atrcepolnes@inbox.lv>
2017-09-14 02:23:58 +0100

    Added Navbar, fade animations, other design elements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,8 @@
 
 $(function() {
-	// $('.spinner').delay(1000).slideUp(function() {
+	$('.spinner').delay(1000).slideUp(function() {
     	$('#loader').fadeOut(2000);
-	// });
-
-	function fade(pageLoad) {
-    var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-    var min = 0.0;
-    var max = 1;
-    var threshold = 0.01;
-    
-    $(".fade").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + 250;
-      
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")<=min+threshold || pageLoad) {$(this).fadeTo(500,max);}
-      }
-    });
-  } fade(true); //fade elements on page-load
-  $(window).scroll(function(){fade(false);}); //fade elements on scroll
-
-    $('.chart').easyPieChart({
-        scaleColor: "#e8eaf6",
-        lineWidth: 12,
-        lineCap: 'round',
-        barColor: '#536dfe',
-        trackColor: "#ffffff",
-        size: 160,
-        animate: 500
-    });
+	});
 
     var $grid = $('.grid').isotope({
         itemSelector: '.grid-item',
@@ -42,7 +14,7 @@ $(function() {
     	$grid.isotope({ filter: toFilter });
     });
 
-    $('a[href="#about"], a[href="#portfolio"]').on('click', function(event) {
+    $('a[href="#about"], a[href="#skills"], a[href="#blog"], a[href="#portfolio"], a[href="#contact"], a[href="#landing"]').on('click', function(event) {
 	    var target = $(this.getAttribute('href'));
 	    if( target.length ) {
 	        event.preventDefault();
@@ -50,5 +22,32 @@ $(function() {
 	            scrollTop: target.offset().top
 	        }, 1000);
 	    }
+	});
+
+	window.sr = ScrollReveal();
+	sr.reveal('.scrollreveal', {duration: 1000,});
+	sr.reveal('.graphs', { beforeReveal: function(el) {
+      $('.chart').easyPieChart({
+	        scaleColor: "#e8eaf6",
+	        lineWidth: 12,
+	        lineCap: 'round',
+	        barColor: '#536dfe',
+	        trackColor: "#ffffff",
+	        size: 160,
+	        animate: 2000
+	    });
+    }}, 250);
+    sr.reveal('.each', 250);
+
+
+    $( window ).scroll(function() {
+    	if ($( window ).scrollTop() > 0) {
+    		$('nav').addClass('scrolled-nav');
+    		$('#go-to-blog, #centered-nav > li > a, #navbar-brand').addClass('scrolled-nav-color');
+    	} else {
+    		$('nav').removeClass('scrolled-nav');
+    		$('#go-to-blog, #centered-nav > li > a, #navbar-brand').removeClass('scrolled-nav-color');
+
+    	}
 	});
 });
